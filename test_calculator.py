f7f5c762b591295d62b5b7eaa9db03213ec0e4e2
litwor1 <rzepeckapaulina@gmail.com>
2025-11-03 19:35:49 +0100

    test_calculator - update to floats testing

--- test_calculator.py
+++ test_calculator.py
@@ -8,39 +8,32 @@ c4 = Calculator(2, 1)
 c5 = Calculator(2, 0)
 
 
-@pytest.mark.parametrize("c, result",
-                         [(c1, 3),
-                          (c2, 2334566569),
-                          (c3, -53243.000004),
-                          (c4, 3)
-                          ])
-def test_add(c, result):
-    assert c.sum() == result
+@pytest.mark.parametrize(
+    "c, result", [(c1, 3), (c2, 2334566569), (c3, -53243.000004), (c4, 3), (c5, 2)]
+)
+def test_add(c: Calculator, result: int):
+    assert pytest.approx(c.sum()) == result
 
 
-@pytest.mark.parametrize("c, result",
-                         [(c1, -1),
-                          (c2, -2334566561),
-                          (c3, -53242.999996),
-                          (c4, 1)
-                          ])
+@pytest.mark.parametrize(
+    "c, result", [(c1, -1), (c2, -2334566561), (c3, -53242.999996), (c4, 1), (c5, 2)]
+)
 def test_subtract(c, result):
-    assert c.subtract() == result
+    assert pytest.approx(c.subtract()) == result
 
 
-@pytest.mark.parametrize("c, result",
-                         [(c1, 2),
-                          (c2, 9338266260),
-                          (c4, 2)
-                          ])
+@pytest.mark.parametrize("c, result", [(c1, 2), (c2, 9338266260), (c4, 2), (c5, 0)])
 def test_multiply(c, result):
-    assert c.multiply() == result
+    assert pytest.approx(c.multiply()) == result
 
 
-@pytest.mark.parametrize("c, result",
-                         [(c1, 0.5),
-                          (c4, 2),
-                          (c5, None),
-                          ])
+@pytest.mark.parametrize(
+    "c, result",
+    [
+        (c1, 0.5),
+        (c4, 2),
+        (c5, None),
+    ],
+)
 def test_divide(c, result):
-    assert c.divide() == result
+    assert pytest.approx(c.divide()) == result
