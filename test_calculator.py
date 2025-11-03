35e63c14fc96934824fc91ec21cd7fc499b55947
litwor1 <rzepeckapaulina@gmail.com>
2025-11-03 20:08:08 +0100

    test_calculator - refactoring to avoid global Calculator objects

--- test_calculator.py
+++ test_calculator.py
@@ -1,39 +1,61 @@
 import pytest
 from calculator import Calculator
 
-c1 = Calculator(1, 2)
-c2 = Calculator(4, 2334566565)
-c3 = Calculator(-53243, -0.000004)
-c4 = Calculator(2, 1)
-c5 = Calculator(2, 0)
+# c1 = Calculator(1, 2)
+# c2 = Calculator(4, 2334566565)
+# c3 = Calculator(-2334566565, -0.000004)
+# c4 = Calculator(2, 1)
+# c5 = Calculator(2, 0)
 
 
 @pytest.mark.parametrize(
-    "c, result", [(c1, 3), (c2, 2334566569), (c3, -53243.000004), (c4, 3), (c5, 2)]
+    "a, b, result",
+    [
+        (1, 2, 3),
+        (4, 2334566565, 2334566569),
+        (-0.1, -0.2, -0.3),
+        (2, 1, 3),
+        (2, 0, 2),
+    ],
 )
-def test_add(c: Calculator, result: int):
+def test_add(a: int, b: int, result: int):
+    c = Calculator(a, b)
     assert pytest.approx(c.sum()) == result
 
 
 @pytest.mark.parametrize(
-    "c, result", [(c1, -1), (c2, -2334566561), (c3, -53242.999996), (c4, 1), (c5, 2)]
+    "a, b, result",
+    [
+        (1, 2, -1),
+        (4, 2334566565, -2334566561),
+        (-0.1, -0.2, 0.1),
+        (2, 1, 1),
+        (2, 0, 2),
+    ],
 )
-def test_subtract(c, result):
+def test_subtract(a: int, b: int, result):
+    c = Calculator(a, b)
     assert pytest.approx(c.subtract()) == result
 
 
-@pytest.mark.parametrize("c, result", [(c1, 2), (c2, 9338266260), (c4, 2), (c5, 0)])
-def test_multiply(c, result):
+@pytest.mark.parametrize(
+    "a, b, result",
+    [(1, 2, 2), (4, 2334566565, 9338266260), (-0.1, -0.2, 0.02), (2, 1, 2), (2, 0, 0)],
+)
+def test_multiply(a: int, b: int, result):
+    c = Calculator(a, b)
     assert pytest.approx(c.multiply()) == result
 
 
 @pytest.mark.parametrize(
-    "c, result",
+    "a,b, result",
     [
-        (c1, 0.5),
-        (c4, 2),
-        (c5, None),
+        (1, 2, 0.5),
+        (2, 1, 2),
+        (-0.1, -0.2, 0.5),
+        (2, 0, None),
     ],
 )
-def test_divide(c, result):
+def test_divide(a: int, b: int, result):
+    c = Calculator(a, b)
     assert pytest.approx(c.divide()) == result
