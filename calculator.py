d3dafcef74ec4aa763c3efcaff3f5f1c35adae67
litwor1 <rzepeckapaulina@gmail.com>
2025-11-03 20:11:35 +0100

    Calculator - Update to divide method to properly raise ZeroDivisionError

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+
 class Calculator:
     def __init__(self, op1: float, op2: float):
         self.__op1 = op1
@@ -14,7 +15,5 @@ class Calculator:
     def multiply(self) -> float:
         return self.__op1 * self.__op2
 
-    def divide(self) -> Optional[float]:
-        if self.__op2 == 0:
-            return None
+    def divide(self) -> float:
         return self.__op1 / self.__op2
