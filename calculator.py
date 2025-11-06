b0baac5e72785ddc98a2049ec91f670781e6fc34
litwor1 <rzepeckapaulina@gmail.com>
2025-11-06 19:48:36 +0100

    ZeroDivisionError management and new coverage report

--- calculator.py
+++ calculator.py
@@ -13,12 +13,23 @@ class Calculator:
         return self.__op1 * self.__op2
 
     def divide(self) -> float:
+        if self.__op2 == 0:
+            raise ZeroDivisionError("Error: Division by zero!")
         return self.__op1 / self.__op2
 
-
+#
 # if __name__ == "__main__":
-#    c = Calculator(2, 3)
-#    print(f"c.sum: {c.sum()}")
-#    print(f"c.subtract: {c.subtract()}")
-#    print(f"c.multiply: {c.multiply()}")
-#    print(f"c.divide: {c.divide()}")
+#     c = Calculator(2, 3)
+#     print(f"c.sum: {c.sum()}")
+#     print(f"c.subtract: {c.subtract()}")
+#     print(f"c.multiply: {c.multiply()}")
+#     print(f"c.divide: {c.divide()}")
+#
+#     c = Calculator(2, 0)
+#     print(f"c.sum: {c.sum()}")
+#     print(f"c.subtract: {c.subtract()}")
+#     print(f"c.multiply: {c.multiply()}")
+#     try:
+#         print(f"c.divide: {c.divide()}")
+#     except ZeroDivisionError:
+#         print("Division by zero is forbidden!")
